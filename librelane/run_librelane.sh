a4bc7efd3df072dc44e97bb5f4b21cbe1a02e02f
Simon Dorrer <simon.dorrer@jku.at>
2025-10-06 11:38:54 +0200

    updated run_librelane.sh --> open OpenROAD GUI in the end

--- librelane/run_librelane.sh
+++ librelane/run_librelane.sh
@@ -3,7 +3,7 @@
 # =====================================================
 # Author: Simon Dorrer
 # Last Modified: 02.10.2025
-# Description: This .sh file switches to the SKY130 PDK and runs the LibreLane flow.
+# Description: This .sh file switches to the SKY130 PDK, runs the LibreLane flow and opens the layout in the OpenROAD GUI.
 # =====================================================
 
 set -e -x
@@ -15,3 +15,6 @@ source sak-pdk-script.sh sky130A sky130_fd_sc_hd > /dev/null
 
 # Run LibreLane
 librelane --manual-pdk config.json
+
+# Open Layout in OpenROAD GUI
+librelane --manual-pdk config.json --last-run --flow OpenInOpenROAD
