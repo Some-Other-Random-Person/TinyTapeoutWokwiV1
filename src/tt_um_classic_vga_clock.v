551ac641a137011bb0d832aa2f8e26b03ea02366
unknown <timo.laimer@aon.at>
2025-11-09 23:34:46 +0100

    name correction

--- src/tt_um_classic_vga_clock.v
+++ src/tt_um_classic_vga_clock.v
@@ -14,7 +14,7 @@
 
 `include "classic_vga_clock.v"
 
-module tt_um_classic_vga_clock_TT (
+module tt_um_classic_vga_clock (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
